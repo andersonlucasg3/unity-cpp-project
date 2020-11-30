81b7a9256d2d1380a6e6c4b17e3e7a25cfcb99dd
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2020-11-29 21:53:11 -0300

    Fixed working on MacOS editor;

--- Assets/UnityCpp/NativeConstants.cs
+++ Assets/UnityCpp/NativeConstants.cs
@@ -4,7 +4,11 @@ namespace UnityCpp
 {
     internal static class NativeConstants
     {
+#if UNITY_EDITOR_WIN
         internal const string nativeCodeAssemblyPath = "Assets/Plugins/NativeComponents/UnityCppLib";
+#else
+        internal const string nativeCodeAssemblyPath = "Assets/Plugins/NativeComponents/libUnityCppLib.dylib";
+#endif
         internal const string nativeLoaderName = "UnityCppAssemblyHelper";
         internal const string nativeGetErrorFuncName = "__GetError";
         internal const string nativeLoadLibraryFuncName = "__LoadLibrary";
