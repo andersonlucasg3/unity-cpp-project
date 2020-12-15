2ad365c5543af4a1b4eda6ecfc3be7ee5aa71802
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2020-12-15 19:07:51 -0300

    Implementing an automatic setup for the Cpp project;

--- Assets/UnityCpp/Editor/NativeProjectSetup.cs
+++ Assets/UnityCpp/Editor/NativeProjectSetup.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.IO;
+using ICSharpCode.SharpZipLib.Zip;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -9,52 +10,103 @@ namespace UnityCpp.Editor
     public static class NativeProjectSetup
     {
         private const string _githubRepoUri = "https://github.com/andersonlucasg3/unity-cpp-project/archive/main.zip";
+        private const string _setupProjectMenuItem = "Assets/UnityCpp Setup Project";
+        private const string _temporaryFolderName = "CppProject";
+        private const string _zipFileName = "github.zip";
+        private const string _extractedDirectoryName = "unity-cpp-project-main";
+        private const string _csharpUnityCppPath = "Assets/UnityCpp";
+
+        private static readonly Dictionary<string, string> _directoriesToCopy = new Dictionary<string, string>
+        {
+            { "CppSource", "/" },
+            { $"{_csharpUnityCppPath}/Loader", _csharpUnityCppPath },
+            { $"{_csharpUnityCppPath}/NativeBridge", _csharpUnityCppPath }
+        };
         
-        [MenuItem("Assets/UnityCpp/Setup Project")]
-        public static void SetupProject()
+        [MenuItem(_setupProjectMenuItem)]
+        private static void SetupProject()
         {
             Debug.Log("---->>> Beginning project setup");
             Debug.Log($"Downloading Cpp Project from repository path: {_githubRepoUri}");
-            DownloadRepository();
+            
+            string filePath = DownloadRepository();
+            if (string.IsNullOrEmpty(filePath))
+            {
+                Debug.LogError("Download repository failed.");
+                return;
+            }
+            
+            string extractedPath = ExtractRepositoryFile(filePath);
+            if (string.IsNullOrEmpty(extractedPath))
+            {
+                Debug.LogError("Unzip zip file failed.");
+                return;
+            }
+            
+            MoveProjectContent(extractedPath);
         }
 
-        private static void DownloadRepository()
+        private static string DownloadRepository()
         {
+            string repositoryFilePath;
             using (UnityWebRequest request = UnityWebRequest.Get(_githubRepoUri))
             {
                 UnityWebRequestAsyncOperation operation = request.SendWebRequest();
                 while (!operation.isDone)
                 {
-                    if (!EditorUtility.DisplayCancelableProgressBar("Downloading from GitHub", null, operation.progress)) continue;
+                    bool cancelPressed = EditorUtility.DisplayCancelableProgressBar("Downloading from GitHub", $"Downloading from repository Uri: {_githubRepoUri}", operation.progress);
+                    if (!cancelPressed) continue;
                     request.Abort();
                     EditorUtility.ClearProgressBar();
-                    return;
+                    return null;
                 }
-                
-                WriteDownloadedData(request.downloadHandler.data);
+                repositoryFilePath = WriteDownloadedData(request.downloadHandler.data);
             }
-            
             EditorUtility.ClearProgressBar();
+            return repositoryFilePath;
         }
 
-        private static void WriteDownloadedData(byte[] data)
+        private static string WriteDownloadedData(byte[] data)
         {
-            string cachesPath = Path.Combine(Application.temporaryCachePath, "CppProject");
-            
+            string cachesPath = Path.Combine(Application.temporaryCachePath, _temporaryFolderName);
             if (Directory.Exists(cachesPath)) Directory.Delete(cachesPath, true);
             Directory.CreateDirectory(cachesPath);
-            
-            cachesPath = Path.Combine(cachesPath, "github.zip");
-            
+            string cachesFilePath = Path.Combine(cachesPath, _zipFileName);
             Debug.Log($"Writing downloaded content to file at: {cachesPath}");
-            
-            EditorUtility.DisplayProgressBar("Writing to file", null, 0F);
-            
-            File.WriteAllBytes(cachesPath, data);
-            
-            EditorUtility.DisplayProgressBar("Written to file", null, 1F);
-            
-            Application.OpenURL("C:/Users/ander/AppData/Local/Temp/DefaultCompany/unity-cpp-project/CppProject/");
+            File.WriteAllBytes(cachesFilePath, data);
+            return cachesFilePath;
+        }
+
+        private static string ExtractRepositoryFile(string zipPath)
+        {
+            FastZipEvents events = new FastZipEvents();
+            events.Progress += (sender, args) => EditorUtility.DisplayProgressBar("Unzipping", args.Name, args.PercentComplete);
+            FastZip zip = new FastZip(events);
+
+            string targetDirectory = Directory.GetParent(zipPath).ToString();
+            zip.ExtractZip(zipPath, targetDirectory, null);
+
+            Debug.Log($"Extracted repository zip to path: {targetDirectory}");
+            EditorUtility.ClearProgressBar();
+
+            return Path.Combine(targetDirectory, _extractedDirectoryName);
+        }
+
+        private static void MoveProjectContent(string extractedPath)
+        {
+            Debug.Log($"Extracted path: {extractedPath}");
+
+            foreach (KeyValuePair<string, string> pair in _directoriesToCopy)
+            {
+                if (!Directory.Exists(pair.Value))
+                {
+                    Directory.CreateDirectory(pair.Value);
+                }
+
+                string fromFullPath = Path.Combine(extractedPath, pair.Key);
+                string toFullPath = Path.Combine(Directory.GetParent(Application.dataPath).ToString(), pair.Value);
+                Directory.Move(fromFullPath, toFullPath);
+            }
         }
     }
 }
