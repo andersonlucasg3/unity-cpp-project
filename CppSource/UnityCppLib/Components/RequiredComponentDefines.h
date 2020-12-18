3d383f9fb4f750aaa009ec3382b9631a8491567e
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2020-12-18 12:26:19 -0300

    Removed old component classes;

--- CppSource/UnityCppLib/Components/RequiredComponentDefines.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define REGISTER_COMPONENT_CREATE_INSTANCE_DECLARE_METHOD(CLASS) \
-    NativeComponent *CreateInstance_##CLASS() {    \
-        return new CLASS(); \
-    }
-#define REGISTER_COMPONENT_CALL(CLASS) \
-    ComponentFactory::Instance()->RegisterComponent(#CLASS, &CreateInstance_##CLASS);
