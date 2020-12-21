a0ce72a7f55634420d331d0f42749c6469072fdf
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2020-12-21 17:23:45 -0300

    Updated semaphore API to avoid warnings;

--- CppSource/UnityCppLib/UnityAPI/CppEngine/Trash.cpp
+++ CppSource/UnityCppLib/UnityAPI/CppEngine/Trash.cpp
@@ -7,48 +7,54 @@ namespace CppEngine {
     bool Trash::_running = false;
 
     thread Trash::_thread;
-    sem_t Trash::_incinerateSemaphore;
-    sem_t Trash::_addSemaphore;
-    sem_t Trash::_mutex;
+    sem_t *Trash::_incinerateSemaphore;
+    sem_t *Trash::_addSemaphore;
+    sem_t *Trash::_mutex;
 
     void Trash::garbageDisposer() {
         do {
-            sem_wait(&_addSemaphore);
+            sem_wait(_addSemaphore);
 
-            sem_wait(&_mutex);
+            sem_wait(_mutex);
             incinerate();
-            sem_post(&_mutex);
+            sem_post(_mutex);
 
-            sem_post(&_incinerateSemaphore);
+            sem_post(_incinerateSemaphore);
             this_thread::sleep_for(chrono::milliseconds(100));
         } while (_running);
     }
 
     void Trash::setup() {
-        sem_init(&_incinerateSemaphore, 0, SEM_VALUE_MAX);
-        sem_init(&_addSemaphore, 0, 0);
-        sem_init(&_mutex, 0, 1);
+        static string_c incinerateSem = "incinerate semaphore";
+        static string_c addSem = "add semaphore";
+        static string_c mutexSem = "mutex semaphore";
+        _incinerateSemaphore = sem_open(incinerateSem, O_CREAT);
+        _addSemaphore = sem_open(addSem, O_CREAT);
+        _mutex = sem_open(mutexSem, O_CREAT);
+        sem_unlink(incinerateSem);
+        sem_unlink(addSem);
+        sem_unlink(mutexSem);
         _running = true;
         _thread = thread(&garbageDisposer);
     }
 
     void Trash::add(Object *obj) {
-        sem_wait(&_incinerateSemaphore);
+        sem_wait(_incinerateSemaphore);
 
-        sem_wait(&_mutex);
+        sem_wait(_mutex);
         _trashBag.push(obj);
-        sem_post(&_mutex);
+        sem_post(_mutex);
 
-        sem_post(&_addSemaphore);
+        sem_post(_addSemaphore);
     }
 
     void Trash::empty() {
         _running = false;
         _thread.join();
 
-        sem_destroy(&_incinerateSemaphore);
-        sem_destroy(&_addSemaphore);
-        sem_destroy(&_mutex);
+        sem_close(_incinerateSemaphore);
+        sem_close(_addSemaphore);
+        sem_close(_mutex);
 
         incinerate();
     }
